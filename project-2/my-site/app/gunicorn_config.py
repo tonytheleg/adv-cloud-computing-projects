09f6c97676c42ffb9a98606fd61f2c740d872957
tonytheleg <anatale@protonmail.com>
2020-06-20 11:11:49 -0400

    start of project 2

---
+++ project-2/my-site/app/gunicorn_config.py
@@ -0,0 +1,15 @@
+pidfile = 'mysite.pid'
+worker_tmp_dir = '/dev/shm'
+worker_class = 'gthread'
+workers = 2
+worker_connections = 1000
+timeout = 30
+keepalive = 2
+threads = 4
+proc_name = 'mysite'
+bind = '0.0.0.0:8080'
+backlog = 2048
+accesslog = '-'
+errorlog = '-'
+user = 'ubuntu'
+group = 'ubuntu'
